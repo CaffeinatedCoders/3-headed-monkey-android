70218c52ebeaf038dc16c95f869601745008ad64
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-10-21 17:23:14 +0200

    add basic phonenumber classes

--- three-headed-monkey/src/net/three_headed_monkey/ThreeHeadedMonkeyApplication.java
+++ three-headed-monkey/src/net/three_headed_monkey/ThreeHeadedMonkeyApplication.java
@@ -1,15 +1,19 @@
 package net.three_headed_monkey;
 
 import android.app.Application;
+import net.three_headed_monkey.data.PhoneNumberSettings;
 import net.three_headed_monkey.data.SimCardSettings;
 
 public class ThreeHeadedMonkeyApplication extends Application {
     public SimCardSettings simCardSettings;
+    public PhoneNumberSettings phoneNumberSettings;
 
     @Override
     public void onCreate() {
         super.onCreate();
         simCardSettings = new SimCardSettings(this);
         simCardSettings.load();
+        phoneNumberSettings = new PhoneNumberSettings(this);
+        phoneNumberSettings.loadSettings();
     }
 }
---
+++ three-headed-monkey/src/net/three_headed_monkey/data/PhoneNumberInfo.java
@@ -0,0 +1,5 @@
+package net.three_headed_monkey.data;
+
+public class PhoneNumberInfo {
+    public String phoneNumber;
+}
