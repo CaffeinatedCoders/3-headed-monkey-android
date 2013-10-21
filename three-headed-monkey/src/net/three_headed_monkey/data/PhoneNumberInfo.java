fce29700618f4e3775ed0fb47e7cced9bb6a8368
Thomas Rosmarin <thomas.rosmarin@gmail.com>
2013-10-21 17:32:37 +0200

    add equals for PhoneNumberInfo

--- three-headed-monkey/src/net/three_headed_monkey/data/PhoneNumberInfo.java
+++ three-headed-monkey/src/net/three_headed_monkey/data/PhoneNumberInfo.java
@@ -2,4 +2,14 @@ package net.three_headed_monkey.data;
 
 public class PhoneNumberInfo {
     public String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if(o==this)
+            return true;
+        if(!o.getClass().equals(PhoneNumberInfo.class))
+            return false;
+        PhoneNumberInfo phoneNumberInfo = (PhoneNumberInfo)o;
+        return phoneNumberInfo.phoneNumber.equals(phoneNumber);
+    }
 }
