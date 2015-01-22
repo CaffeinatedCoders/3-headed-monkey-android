c3f37ee8e7f4014b1001e93d36ad2e02e98ae2bb
xorrizon <xorrizon@gmail.com>
2015-01-22 14:57:23 +0100

    Fixed Robolectric Multidex Issue
    Added an empty shadow for MultiDex since we do not need dexing with robolectric anyway

---
+++ three-headed-monkey/src/test/java/net/three_headed_monkey/custom_shadows/ShadowMultiDex.java
@@ -0,0 +1,18 @@
+//copied from https://raw.githubusercontent.com/robolectric/robolectric/master/robolectric-shadows/shadows-multidex/src/main/java/org/robolectric/shadows/ShadowMultiDex.java
+//@TODO: can be removed once this class makes it into robolectric release
+
+package net.three_headed_monkey.custom_shadows;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import org.robolectric.annotation.Implementation;
+import org.robolectric.annotation.Implements;
+
+@Implements(MultiDex.class)
+public class ShadowMultiDex {
+  @Implementation
+  public static void install(Context context) {
+    // Do nothing since with Robolectric nothing is dexed.
+  }
+}
